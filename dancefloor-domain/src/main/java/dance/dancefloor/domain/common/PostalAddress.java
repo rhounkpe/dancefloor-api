43dbc7e2839225be6d4c3d42a011f5cc6cbb4e1d
Mêtogbé Rufin Hounkpe <rhounkpe@yahoo.fr>
2018-09-03 00:01:11 +0200

    PostalAddress class

---
+++ dancefloor-domain/src/main/java/dance/dancefloor/domain/common/PostalAddress.java
@@ -0,0 +1,10 @@
+package dance.dancefloor.domain.common;
+
+public class PostalAddress {
+    private String addressCountry;
+    private String addressLocality;
+    private String addressRegion;
+    private String postOfficeBoxNumber;
+    private int postalCode;
+    private String streetAddress;
+}
